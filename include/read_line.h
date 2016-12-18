fcb831094c3141d162de36757c6369e7636cb2fe
Chris Nisbet <nischris@gmail.com>
2016-12-19 11:09:58 +1300

    Changed the library name to libioutils.

--- include/read_line.h
+++ include/read_line.h
@@ -1,8 +1,8 @@
-#ifndef __READ_LINE_H__
-#define __READ_LINE_H__
+#ifndef __IO_UTILS_READ_LINE_H__
+#define __IO_UTILS_READ_LINE_H__
 
 #include <stddef.h>
 
 int read_line_with_timeout(char * * output_buffer, size_t * output_buffer_size, int fd, unsigned int timeout_seconds);
 
-#endif /*  __READ_LINE_H__ */
+#endif /*  __IO_UTILS_READ_LINE_H__ */
